3211c5b03f6315d9d544f0dbbf2c317666e911d6
Ethan Shold <sholdee@gmail.com>
2026-05-22 15:17:51 -0500

    fix: skip cluster-scoped namespace normalization

--- internal/app/normalize.go
+++ internal/app/normalize.go
@@ -5,8 +5,25 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+var knownClusterScopedKinds = map[string]struct{}{
+	"/Namespace":        {},
+	"/Node":             {},
+	"/PersistentVolume": {},
+	"admissionregistration.k8s.io/MutatingWebhookConfiguration":   {},
+	"admissionregistration.k8s.io/ValidatingWebhookConfiguration": {},
+	"apiextensions.k8s.io/CustomResourceDefinition":               {},
+	"apiregistration.k8s.io/APIService":                           {},
+	"rbac.authorization.k8s.io/ClusterRole":                       {},
+	"rbac.authorization.k8s.io/ClusterRoleBinding":                {},
+	"scheduling.k8s.io/PriorityClass":                             {},
+	"storage.k8s.io/StorageClass":                                 {},
+}
+
 func ApplyDestinationNamespace(application argoappv1.Application, obj *unstructured.Unstructured) {
 	if obj.GetNamespace() != "" {
+		return
+	}
+	if isKnownClusterScoped(obj) {
 		return
 	}
 	namespace := application.Spec.Destination.Namespace
@@ -14,4 +31,10 @@ func ApplyDestinationNamespace(application argoappv1.Application, obj *unstructu
 		return
 	}
 	obj.SetNamespace(namespace)
+}
+
+func isKnownClusterScoped(obj *unstructured.Unstructured) bool {
+	gvk := obj.GroupVersionKind()
+	_, ok := knownClusterScopedKinds[gvk.Group+"/"+gvk.Kind]
+	return ok
 }
